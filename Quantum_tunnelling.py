0c0ba40a877709b9413ddac07bf89c0a12697fe3
Dominik Furgacz <31513498+chaosdomok@users.noreply.github.com>
2024-11-25 22:16:09 +0100

    Quantum Tunnelling

---
+++ Quantum_tunnelling.py
@@ -0,0 +1,30 @@
+import qiskit
+import matplotlib.pyplot as plt
+
+# Create Quantum Circut with 1 Qubit
+circuit = qiskit.QuantumCircuit(1, 1)
+
+# Molecular initial state simulation
+circuit.h(0) # We apply the Hadamard operator to create a superposition
+
+# Simulation of a "barrier" - we simulate uncertainty about the passage
+circuit.x(0)  # Pauli-X operator acts as a "barrier"
+
+#Particle state measurement
+circuit.measure(0, 0)
+
+# Circut drawing
+circuit.draw('mpl')
+plt.show()
+
+# Symulacja na lokalnym symulatorze Aer
+simulator = qiskit.Aer.get_backend('qasm_simulator')
+result = qiskit.execute(circuit, simulator, shots=1000).result()
+counts = result.get_counts()
+
+# Wizualizacja wyników
+plt.bar(counts.keys(), counts.values())
+plt.xlabel('Stan końcowy')
+plt.ylabel('Liczba pomiarów')
+plt.title('Symulacja tunelowania kwantowego')
+plt.show()
